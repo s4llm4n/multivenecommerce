1301a0758a7707d9821c1512e94b75fb2930fffb
s4llm4n <s4llm4n@gmail.com>
2025-03-10 23:07:28 +0700

    first multivendorecommerce

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">
+        Salman Hatrash
+      </h1>
+    </div>
+  );
+}
+
+export default App;
