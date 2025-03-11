029a9a66be9650d973ad689699cbd5449959a9c2
s4llm4n <s4llm4n@gmail.com>
2025-03-11 10:28:41 +0700

    register

--- src/views/auth/Register.jsx
+++ src/views/auth/Register.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 const Register = () => {
     return (
-        <div>
-            <h1>Register</h1>
+        <div className='min-w-screen min-h-screen bg-[#cdcae9] flex justify-center items-center'>
+            <div className='w-[350px] text-[#ffffff] p-2'>
+                <div className='bg-[#6f68d1] p-4 rounded-md'>
+                    <h2 className='text-xl mb-3 font-bold'>
+                        Welcome to Ecommerce
+                    </h2>
+                    <p>Please Register your account</p>
+                </div>
+            </div>
         </div>
     );
 };
