2aabc2ede59b5d8fa91b12ad57f76bf60051d837
s4llm4n <s4llm4n@gmail.com>
2025-03-12 19:10:31 +0700

    create api

--- src/views/auth/AdminLogin.jsx
+++ src/views/auth/AdminLogin.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { admin_login } from '../../store/Reducers/authReducer';
 
 const AdminLogin = () => {
+
+    const dispatch = useDispatch()
 
     const [state, setState] = useState({
             email: '',
@@ -16,7 +20,8 @@ const AdminLogin = () => {
     
         const submit = (e) => {
             e.preventDefault()
-            console.log(state)
+            dispatch(admin_login(state))
+            // console.log(state)
         }
 
     return (
