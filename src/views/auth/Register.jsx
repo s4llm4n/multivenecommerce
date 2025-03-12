43cf65d7019fb74913e90f5071013107967f5df3
s4llm4n <s4llm4n@gmail.com>
2025-03-12 09:23:23 +0700

    prepare redux toolkit

--- src/views/auth/Register.jsx
+++ src/views/auth/Register.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { FaGoogle } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
 
 const Register = () => {
+
+    const [state, setState] = useState({
+        name: '',
+        email: '',
+        password: ''
+    })
+
+    const inputHandle = (e) => {
+        setState({
+            ...state,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    const submit = (e) => {
+        e.preventDefault()
+        console.log(state)
+    }
+
     return (
         <div className='min-w-screen min-h-screen bg-[#cdcae9] flex justify-center items-center'>
             <div className='w-[350px] text-[#ffffff] p-2'>
@@ -11,22 +32,22 @@ const Register = () => {
                     </h2>
                     <p className='text-sm mb-3 font-medium'>Please Register your account</p>
 
-                    <form>
+                    <form onSubmit={submit}>
                         <div className='flex flex-col w-full gap-1 mb-3'>
                             <label htmlFor='name'>Name</label>
-                            <input className='px-3 py-2 outline-none border border-slate-400 bg-transparent rounded-md' type='text' name='name' placeholder='Name' id='name' required/>
+                            <input onChange={inputHandle} value={state.name} className='px-3 py-2 outline-none border border-slate-400 bg-transparent rounded-md' type='text' name='name' placeholder='Name' id='name' required/>
 
                         </div>
 
                         <div className='flex flex-col w-full gap-1 mb-3'>
                             <label htmlFor='email'>Email</label>
-                            <input className='px-3 py-2 outline-none border border-slate-400 bg-transparent rounded-md' type='text' name='email' placeholder='Email' id='email' required/>
+                            <input onChange={inputHandle} value={state.email} className='px-3 py-2 outline-none border border-slate-400 bg-transparent rounded-md' type='email' name='email' placeholder='Email' id='email' required/>
 
                         </div>  
 
                         <div className='flex flex-col w-full gap-1 mb-3'>
                             <label htmlFor='password'>Password</label>
-                            <input className='px-3 py-2 outline-none border border-slate-400 bg-transparent rounded-md' type='text' name='password' placeholder='Password' id='password' required/>
+                            <input onChange={inputHandle} value={state.password} className='px-3 py-2 outline-none border border-slate-400 bg-transparent rounded-md' type='password' name='password' placeholder='Password' id='password' required/>
                         </div>
 
                         <div className='flex items-center w-full gap-3 mb-3'>
@@ -44,7 +65,17 @@ const Register = () => {
                             </div>
                             <div className='w-[45%] bg-slate-700 h-[1px]'></div>
                         </div>
-                        <div> </div>
+                        <div className='flex justify-center items-center gap-3'>
+                            <div className='w-[135px] h-[35px] flex rounded-md 
+                            bg-orange-700 shadow-lg hover:shadow-orange-700/50 justify-center cursor-pointer items-center overflow-hidden'>
+                                <span><FaGoogle/></span>
+                            </div>
+
+                            <div className='w-[135px] h-[35px] flex rounded-md 
+                            bg-blue-700 shadow-lg hover:shadow-blue-700/50 justify-center cursor-pointer items-center overflow-hidden'>
+                                <span><FaFacebook/></span>
+                            </div>
+                        </div>
                     </form>
 
                 </div>
