073461f504638581ce448ca75c34b27a7cb9c96c
s4llm4n <s4llm4n@gmail.com>
2025-05-19 22:37:17 +0700

    sellersidebardyn

--- src/store/Reducers/authReducer.js
+++ src/store/Reducers/authReducer.js
@@ -1,5 +1,6 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import api from '../../api/api';
+import { jwtDecode } from 'jwt-decode';
 
 export const admin_login = createAsyncThunk(
     'auth/admin_login', 
@@ -7,7 +8,7 @@ export const admin_login = createAsyncThunk(
         console.log(info)
         try {
             const {data} = await api.post('/admin-login',info,{withCredentials: true})
-            localStorage.setItem('accesToken',data.token)
+            localStorage.setItem('accessToken',data.token)
                 // console.log(data)
                 return fulfillWithValue(data)
         } catch (error) {
@@ -20,11 +21,26 @@ export const admin_login = createAsyncThunk(
 export const seller_login = createAsyncThunk(
     'auth/seller_login', 
     async(info,{rejectWithValue, fulfillWithValue}) => {
-        console.log(info)
+         console.log(info)
         try {
             const {data} = await api.post('/seller-login',info,{withCredentials: true})
             console.log(data)
-            localStorage.setItem('accesToken',data.token)
+            localStorage.setItem('accessToken',data.token)
+            return fulfillWithValue(data)
+        } catch (error) {
+            // console.log(error.response.data)
+            return rejectWithValue(error.response.data)
+        }
+    }
+)
+
+
+export const get_user_info = createAsyncThunk(
+    'auth/get_user_info', 
+    async(_ ,{rejectWithValue, fulfillWithValue}) => {
+        try {
+            const {data} = await api.get('/get-user',{withCredentials: true})
+            // console.log(data)
             return fulfillWithValue(data)
         } catch (error) {
             // console.log(error.response.data)
@@ -40,7 +56,7 @@ export const seller_register = createAsyncThunk(
             console.log(info)
             const {data} = await api.post('/seller-register',info,
                 {withCredentials: true})
-                localStorage.setItem('accesToken',data.token)
+                localStorage.setItem('accessToken',data.token)
                 // console.log(data)
                 return fulfillWithValue(data)
         } catch (error) {
@@ -50,13 +66,32 @@ export const seller_register = createAsyncThunk(
     }
 )
 
+
+    const returnRole = (token) => {
+        if (token) {
+            const decodeToken = jwtDecode(token)
+            const expireTime = new Date(decodeToken.exp * 1000)
+            if (new Date() > expireTime) {
+                localStorage.removeItem('accessToken')
+                return ''
+            } else {
+                return decodeToken.role
+            }
+        } else {
+            return ''
+        }
+    }
+
+
 export const authReducer = createSlice({
     name: 'auth',
     initialState:{
         successMessage: '',
         errorMessage: '',
         loader: false,
-        userInfo: ''
+        userInfo: '',
+        role: returnRole(localStorage.getItem('accessToken')),
+        token: localStorage.getItem('accessToken')
     },
     reducers : {
 
@@ -77,6 +112,22 @@ export const authReducer = createSlice({
         .addCase(admin_login.fulfilled, (state, { payload }) => {
             state.loader = false;
             state.successMessage = payload.message
+            state.token = payload.token
+            state.role = returnRole(payload.token)
+        })
+
+        .addCase(seller_login.pending, (state, { payload }) => {
+            state.loader = true;
+        })
+        .addCase(seller_login.rejected, (state, { payload }) => {
+            state.loader = false;
+            state.errorMessage = payload.error
+        })
+        .addCase(seller_login.fulfilled, (state, { payload }) => {
+            state.loader = false;
+            state.successMessage = payload.message
+            state.token = payload.token
+            state.role = returnRole(payload.token)
         })
 
         .addCase(seller_register.pending, (state, { payload }) => {
@@ -89,6 +140,13 @@ export const authReducer = createSlice({
         .addCase(seller_register.fulfilled, (state, { payload }) => {
             state.loader = false;
             state.successMessage = payload.message
+            state.token = payload.token
+            state.role = returnRole(payload.token)
+        })
+
+        .addCase(get_user_info.fulfilled, (state, { payload }) => {
+            state.loader = false;
+            state.userInfo = payload.userInfo
         })
     }
 })
