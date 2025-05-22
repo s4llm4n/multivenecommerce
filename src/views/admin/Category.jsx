5d43beb482fd3dcca86322494a9ff14304c54c8e
s4llm4n <s4llm4n@gmail.com>
2025-05-22 21:45:10 +0700

    commit searchv

--- src/views/admin/Category.jsx
+++ src/views/admin/Category.jsx
@@ -1,18 +1,20 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Pagination from '../Pagination';
 import { FaEdit, FaTrash, FaImage } from 'react-icons/fa';
 import { IoMdCloseCircle } from "react-icons/io";
 import { PropagateLoader } from 'react-spinners';
 import { overrideStyle } from '../../utils/utils';
-import { categoryAdd } from '../../store/Reducers/categoryReducer';
+import { categoryAdd, messageClear } from '../../store/Reducers/categoryReducer';
 import { useDispatch, useSelector } from 'react-redux';
+import toast from 'react-hot-toast';
+import Search from '../components/Search';
 
 const Category = () => {
 
 
         const dispatch = useDispatch()
-        const {loader} = useSelector(state=> state.category)
+        const {loader,successMessage,errorMessage} = useSelector(state=> state.category)
 
 
         const [currentPage, setCurrentPage] = useState(1)
@@ -45,6 +47,28 @@ const Category = () => {
         }
 
 
+        useEffect(() => {
+                    
+                    if (successMessage) {
+                        toast.success(successMessage)
+                        dispatch(messageClear()) 
+                        setState({
+                            name: '',
+                            image: ''
+                        })
+                        setImage('')
+                    }
+                    if (errorMessage) {
+                        toast.error(errorMessage)
+                        dispatch(messageClear())
+                    }
+                },[successMessage,errorMessage])
+
+
+                useEffect(() => {
+
+                },[searchValue, currentPage, parPage])
+
     return (
         <div className='px-2 lg:px-7 pt-5'>
 
@@ -57,14 +81,8 @@ const Category = () => {
                 <div className='w-full lg:w-7/12'>
                     <div className='w-full p-4 bg-[#6a5fdf] rounded-md'>
 
-                    <div className='flex justify-between items-center'>
-                    <select onChange={(e) => setParPage(parseInt(e.target.value))} className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]'>
-                        <option value='5'>5</option>
-                        <option value='10'>10</option>
-                        <option value='25'>25</option>
-                    </select>
-                    <input className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]' type='text' placeholder='Search..'/>
-                </div>
+                    <Search setParPage={setParPage} setSearchValue={setSearchValue} searchValue={searchValue} />
+
                 <div className='relative overflow-x-auto'>
                     <table className='w-full text-sm text-left text-[#d0d2d6]'>
                         <thead className='text-sm text-[#d0d2d6] uppercase border-b border-slate-700'>
