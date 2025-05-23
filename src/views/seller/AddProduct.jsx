7979b12d2e872e6bef5493eaaa36bcee84539d29
s4llm4n <s4llm4n@gmail.com>
2025-05-23 21:24:56 +0700

    addproduct

--- src/views/seller/AddProduct.jsx
+++ src/views/seller/AddProduct.jsx
@@ -1,36 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { IoImages } from "react-icons/io5";
 import { IoMdCloseCircle } from "react-icons/io";
+import { useDispatch, useSelector } from 'react-redux';
+import { get_category } from '../../store/Reducers/categoryReducer';
 
 const AddProduct = () => {
-    const categorys = [
-        {
-            id: 1,
-            name: 'Sports'
-        },
-        {
-            id: 2,
-            name: 'Tshirt'
-        },
-        {
-            id: 3,
-            name: 'Mobile'
-        },
-        {
-            id: 4,
-            name: 'Computer'
-        },
-        {
-            id: 5,
-            name: 'Watch'
-        },
-        {
-            id: 6,
-            name: 'Pant'
-        }
-    ]
+    const dispatch = useDispatch()
+    const { categorys } = useSelector(state => state.category)
 
+    useEffect(() => {
+        dispatch(get_category({
+            searchValue: '',
+            parPage: '',
+            page: ""
+        }))
+    }, [])
 
     const [state, setState] = useState({
         name: '',
@@ -50,7 +35,7 @@ const AddProduct = () => {
 
     const [cateShow, setCateShow] = useState(false)
     const [category, setCategory] = useState('')
-    const [allCategory, setAllCategory] = useState(categorys)
+    const [allCategory, setAllCategory] = useState([])
     const [searchValue, setSearchValue] = useState('')
 
     const categorySearch = (e) => {
@@ -102,6 +87,15 @@ const AddProduct = () => {
         setImageShow(filterImageUrl)
     }
 
+
+    const add = (e) => {
+        e.preventDefault()
+    }
+
+    useEffect(() => {
+        setAllCategory(categorys)
+    },[categorys])
+
     return (
         <div className='px-2 lg:px-7 pt-5'>
             <div className='w-full p-4 bg-[#6a5fdf] rounded-md'>
@@ -112,7 +106,7 @@ const AddProduct = () => {
                     <Link to='/seller/dashboard/products' className='bg-blue-500 hover:shadow-blue-500/50 hover:shadow-lg text-white rounded-sm px-7 py-2 my-2'>All Product</Link>
                 </div>
                 <div>
-                    <form>
+                    <form onSubmit={add}>
                         <div className='flex flex-col mb-3 md:flex-row gap-4 w-full text-[#d0d2d6]'>
                             <div className='flex flex-col w-full gap-1'>
                                 <label htmlFor='name'>Product Name</label>
