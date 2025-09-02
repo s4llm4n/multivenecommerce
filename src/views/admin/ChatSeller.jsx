ac0bac5b53f12fb17fb90c7323ce2328635a9342
s4llm4n <s4llm4n@gmail.com>
2025-09-02 15:57:20 +0700

    nextadminseller

--- src/views/admin/ChatSeller.jsx
+++ src/views/admin/ChatSeller.jsx
@@ -27,16 +27,16 @@ const ChatSeller = () => {
 
                             {
                                 sellers.map((s,i) => <div className={`h-[60px] flex justify-start gap-2 items-center text-white px-2 py-2 rounded-md cursor-pointer bg-[#8288ed] `}>
-            <div className='relative'>
-             <img className='w-[38px] h-[38px] border-white border-2 max-w-[38px] p-[2px] rounded-full' src={s.image} alt="" />
-             <div className='w-[10px] h-[10px] bg-green-500 rounded-full absolute right-0 bottom-0'></div>
-            </div>
-            <div className='flex justify-center items-start flex-col w-full'>
-                <div className='flex justify-between items-center w-full'>
-                    <h2 className='text-base font-semibold'>{s.name}</h2>
-                </div>
-                </div>
-                </div>
+                                <div className='relative'>
+                                <img className='w-[38px] h-[38px] border-white border-2 max-w-[38px] p-[2px] rounded-full' src={s.image} alt="" />
+                                <div className='w-[10px] h-[10px] bg-green-500 rounded-full absolute right-0 bottom-0'></div>
+                                </div>
+                                <div className='flex justify-center items-start flex-col w-full'>
+                                    <div className='flex justify-between items-center w-full'>
+                                        <h2 className='text-base font-semibold'>{s.name}</h2>
+                                    </div>
+                                    </div>
+                                    </div>
                 )
                     }
                         </div>
