bd50b92f4171f910cd82faf295f01a74bcb1ff07
s4llm4n <s4llm4n@gmail.com>
2025-09-10 14:09:16 +0700

    seller_show

--- src/store/Reducers/chatReducer.js
+++ src/store/Reducers/chatReducer.js
@@ -168,6 +168,9 @@ export const chatReducer = createSlice({
             state.seller_admin_message = payload.messages
             state.currentSeller = payload.currentSeller
         })
+        .addCase(get_seller_message.fulfilled, (state, { payload }) => {
+            state.seller_admin_message = payload.messages
+        })
     }
 })
 
--- src/views/seller/SellerToAdmin.jsx
+++ src/views/seller/SellerToAdmin.jsx
@@ -43,45 +43,46 @@ const SellerToAdmin = () => {
 
                         <div className='py-4'>
                             <div className='bg-[#475569] h-[calc(100vh-290px)] rounded-md p-3 overflow-y-auto'>
-                                <div className='w-full flex justify-start items-center'>
-                                <div className='flex justify-start items-start gap-2 md:px-3 py-2 max-w-full lg:max-w-[85%]'>
-                                    <div>
-                                        <img  className='w-[38px] h-[38px] border-2 border-white rounded-full max-w-[38px] p-[3px]' src='http://localhost:3000/images/demo.jpg' alt=''/>
-                                    </div>
-                                    <div className='flex justify-center items-start flex-col w-full bg-blue-500 shadow-lg shadow-blue-500/50 text-white py-1 px-2 rounded-sm'>
-                                        <span>How Are u?</span>
-                                    </div>
-                                </div>
-                                </div>
 
-                                <div className='w-full flex justify-end items-center'>
-                                <div className='flex justify-start items-start gap-2 md:px-3 py-2 max-w-full lg:max-w-[85%]'>
-                                    <div className='flex justify-center items-start flex-col w-full bg-red-500 shadow-lg shadow-red-500/50 text-white py-1 px-2 rounded-sm'>
-                                        <span>How Are u?</span>
-                                    </div>
-                                        <div>
-                                        <img  className='w-[38px] h-[38px] border-2 border-white rounded-full max-w-[38px] p-[3px]' src='http://localhost:3000/images/admin.jpg' alt=''/>
-                                    </div>
-                                </div>
+                                {
+                                    seller_admin_message.map((m, i) => {
+                                        if (userInfo._id === m.senderId) {
+                                            return (
+                                                <div key={i} className='w-full flex justify-start items-center'>
+                                                <div className='flex justify-start items-start gap-2 md:px-3 py-2 max-w-full lg:max-w-[85%]'>
+                                                    <div>
+                                                        <img  className='w-[38px] h-[38px] border-2 border-white rounded-full max-w-[38px] p-[3px]' src='http://localhost:3000/images/demo.jpg' alt=''/>
+                                                    </div>
+                                                    <div className='flex justify-center items-start flex-col w-full bg-blue-500 shadow-lg shadow-blue-500/50 text-white py-1 px-2 rounded-sm'>
+                                                        <span>{m.message}</span>
+                                                    </div>
+                                                </div>
+                                                </div>
+                                            )
+                                        } else {
+                                            return (
+                                                <div key={i} className='w-full flex justify-end items-center'>
+                                                <div className='flex justify-start items-start gap-2 md:px-3 py-2 max-w-full lg:max-w-[85%]'>
+                                                    <div className='flex justify-center items-start 
+                                                    flex-col w-full bg-red-500 shadow-lg shadow-red-500/50 
+                                                    text-white py-1 px-2 rounded-sm'>
+                                                        <span>{m.message}</span>
+                                                    </div>
+                                                        <div>
+                                                        <img  className='w-[38px] h-[38px] border-2 border-white rounded-full max-w-[38px] p-[3px]' src='http://localhost:3000/images/admin.jpg' alt=''/>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            )
+                                        }
+                                    })
+                                }
                             </div>
-                            <div className='w-full flex justify-start items-center'>
-                                <div className='flex justify-start items-start gap-2 md:px-3 py-2 max-w-full lg:max-w-[85%]'>
-                                    <div>
-                                <img  className='w-[38px] h-[38px] border-2 border-white rounded-full max-w-[38px] p-[3px]' src='http://localhost:3000/images/demo.jpg' alt=''/>
-                                </div>
-                                    <div className='flex justify-center items-start flex-col w-full bg-blue-500 shadow-lg shadow-blue-500/50 text-white py-1 px-2 rounded-sm'>
-                                        <span>I need some help?</span>
-                                    </div>
-                                </div>
-                                </div>
-                                    </div>
-                                </div>
-
+                        </div>
                         <form onSubmit={send} className='flex gap-3'>
                             <input value={text} onChange={(e) => setText(e.target.value)} className='w-full flex justify-between px-2 border border-slate-700 items-center py-[5px] focus:border-blue-500 rounded-md outline-none bg-transparent text-[#d0d2d6]' type="text" placeholder='Input Your Message' />
                             <button className='shadow-lg bg-[#06b6d4] hover:shadow-cyan-500/50 text-semibold w-[75px] h-[35px] rounded-md text-white flex justify-center items-center'>Send</button>
                         </form>
-
                     </div>
                 </div>
             </div>
