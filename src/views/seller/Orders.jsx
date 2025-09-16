5321215fb7666bd606d8de9ffc4512aea844a59e
s4llm4n <s4llm4n@gmail.com>
2025-09-16 15:58:12 +0700

    sellerorderkeep

--- src/views/seller/Orders.jsx
+++ src/views/seller/Orders.jsx
@@ -9,6 +9,7 @@ import { get_seller_orders } from '../../store/Reducers/OrderReducer';
 const Orders = () => {
 
     const dispatch = useDispatch()
+    
     const {myOrders,totalOrder} = useSelector(state => state.order)
     const {userInfo} = useSelector(state => state.auth)
 
@@ -40,20 +41,22 @@ const Orders = () => {
                             <th scope='col' className='py-3 px-4'>Price</th>
                             <th scope='col' className='py-3 px-4'>Payment Status</th>
                             <th scope='col' className='py-3 px-4'>Order Status</th>
+                            <th scope='col' className='py-3 px-4'>Date</th>
                             <th scope='col' className='py-3 px-4'>Action</th>
                         </tr>
                         </thead>
 
                         <tbody>
                             {
-                                [1,2,3,4,5].map((d, i) => <tr key={i}>
-                                    <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>#5454</td>
-                                    <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>$455</td>
-                                    <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>Pending</td>
-                                    <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>Pending</td>
+                                myOrders.map((d, i) => <tr key={i}>
+                                    <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>#{d._id}</td>
+                                    <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>${d.price}</td>
+                                    <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>{d.payment_status}</td>
+                                    <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>{d.delivery_status}</td>
+                                    <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>{d.date}</td>
                                     <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>
                                         <div className='flex justify-start items-center gap-4'>
-                                        <Link to={`/seller/dashboard/orders/details/34`} className='p-[6px] bg-green-500 rounded hover:shadow-lg hover:shadow-green-500/50'><FaEye/></Link>
+                                        <Link to={`/seller/dashboard/orders/details/${d._id}`} className='p-[6px] bg-green-500 rounded hover:shadow-lg hover:shadow-green-500/50'><FaEye/></Link>
                                         </div>
                                         </td>      
                                 </tr>)
